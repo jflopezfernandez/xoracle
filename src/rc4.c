0ac18bf7647b899f9f903f82c9846f097bd4e719
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-01-21 17:04:34 -0500

    RC4 cipher now drops the first 3072 bytes from the keystream.

--- src/rc4.c
+++ src/rc4.c
@@ -1,13 +1,21 @@
 
 #include "xoracle.h"
 
+#ifndef RC4_DROP_N
+#define RC4_DROP_N (3072)
+#endif /* RC4_DROP_N */
+
 static void swap_byte(unsigned char* a, unsigned char* b) {
     unsigned char temp = *a;
     *a = *b;
     *b = temp;
 }
 
+/** This function initializes the key-scheduling algorithm
+ *  by scrambling the state vector in the key struct.
+ */
 void prepare_key(unsigned char* key_data_ptr, int key_data_len, rc4_key* key) {
+    /* Initialize to the first element of the state vector */
     unsigned char* state = &key->state[0];
 
     /* Identity permutation */
@@ -34,10 +42,35 @@ void rc4(unsigned char* buffer_ptr, int buffer_len, rc4_key* key) {
 
     unsigned char* state = &key->state[0];
 
+    /** This counter keeps track of the number of bytes that
+     *  have been discarded from the keystream so far. The
+     *  minimum number must be 256, but the recommended
+     *  amount is 3,072, which is the default.
+     */
+    size_t dropped_bytes = 0;
+
     for (int i = 0; i < buffer_len; ++i) {
         x = (x + 1) % 256;
         y = (state[x] + y) % 256;
         swap_byte(&state[x], &state[y]);
+
+        if (dropped_bytes < RC4_DROP_N) {
+            /* Increment the drop count */
+            ++dropped_bytes;
+
+            /** Decrement the loop counter to make sure we
+             *  complete enough iterations to encrypt the
+             *  entire input buffer.
+             */
+            --i;
+
+            /** Move on to the next iteration without
+             *  modifying the output buffer.
+             */
+            continue;
+        }
+
+        /* xor the keystream with the plaintext */
         unsigned char xor_index = (state[x] + state[y]) % 256;
         buffer_ptr[i] ^= state[xor_index];
     }
